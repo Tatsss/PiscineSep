461d5f27ce3f4009cbd3e38a3d548417670276d3
Tatsuya <>
2024-09-09 10:47:14 +0900

    00

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,16 +6,35 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 09:52:40 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/09 09:53:16 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/09 10:13:42 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strcpy(char *dest, char *src)
 {
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
 
 int	main(void)
 {
-	ft_strcpy();
+	char	original[] = "abcdef";
+	char	destination[] = "";
+
+	printf("処理前出力結果：%s\t%s\n", original, destination);
+	strcpy(destination, original);
+	printf("strcpy実行後出力結果：%s\t%s\n", original, destination);
+	ft_strcpy(destination, original);
+	printf("処理後出力結果：%s\t%s\n", original, destination);
 	return (0);
 }
