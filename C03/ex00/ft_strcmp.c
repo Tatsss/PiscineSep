0cfd98f33cf4cd06ac78e9efb480ca93a9f461a3
Tatsuya <>
2024-09-10 02:19:25 +0900

    update

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 00:47:31 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/10 02:05:07 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/10 02:19:01 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,22 @@ int	main(void)
 {
 	char	original[] = "ABCDE";
 	char	same[] = "ABCDE";
-	char	diffplus[] = "BBCDE";
-	char	diffminus[] = "@BCDEF";
+	char	diffoneup[] = "BBCDE";
+	char	diffonedown[] = "@BCDEF";
+	char	null[] = "\0";
+	char	del[] = "\x7f";
 	int		result;
 
 	result = strcmp(original, same);
-	printf("strcmp関数 同じケース\t：%d\n", result);
-	result = strcmp(original, diffplus);
-	printf("strcmp関数 プラスに異なるケース\t：%d\n", result);
-	result = strcmp(original, diffminus);
+	printf("strcmp関数 同じケース\t\t\t：%d\n", result);
+	result = strcmp(original, diffoneup);
 	printf("strcmp関数 マイナスに異なるケース\t：%d\n", result);
+	result = strcmp(original, diffonedown);
+	printf("strcmp関数 プラスに異なるケース\t\t：%d\n", result);
+	result = strcmp(del, null);
+	printf("strcmp関数 最大値プラス\t\t\t：%d\n", result);
+	result = strcmp(null, del);
+	printf("strcmp関数 最小値マイナス\t\t：%d\n\n", result);
 	// ft_strcmp(hoge, fuga);
 	return (0);
 }
