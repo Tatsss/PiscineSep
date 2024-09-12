600be8d08d063bfaff46ce7d952f04c3fc6ad43d
Tatsuya <>
2024-09-12 12:27:08 +0900

    C04 done

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 11:46:28 by tokuda            #+#    #+#             */
+/*   Updated: 2024/09/12 12:26:30 by tokuda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	ans;
+	int	negative;
+
+	i = 0;
+	ans = 0;
+	negative = 1;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == ' ')
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		ans = 10 * ans + (str[i] - '0');
+		i++;
+	}
+	return (ans * negative);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int		result;
+	char	hoge[3] = "123";
+	char	fuga[19] = "   ---+--+1234ab567";
+	char	foo[50] = " 		-++-+--+888fsad982j";
+
+	result = ft_atoi(hoge);
+	printf("%d\n", result);
+	result = ft_atoi(fuga);
+	printf("%d\n", result);
+	result = ft_atoi(foo);
+	printf("%d\n", result);
+	return (0);
+}
