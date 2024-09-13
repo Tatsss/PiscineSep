20227ad7618ae4cc981fa8c807fc32b9506b2cb7
Tatsuya Okuda <hotatsu.world@gmail.com>
2024-09-13 21:55:52 +0900

    aa

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -6,7 +6,7 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:16:05 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/12 13:21:59 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/13 20:41:35 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 void	ft_putstr(char *str)
 {
-	int	i;
+	int		i;
+	char	newline;
 
+	newline = '\n';
 	i = 0;
 	while (str[i])
 	{
 		write(1, &str[i], 1);
-		write(1, '\n', 1);
 		i++;
 	}
+	write(1, &newline, 1);
 }
 
 int	main(int argc, char *argv[])
 {
 	int	i;
-	int	size;
 
 	i = argc - 1;
 	while (i > 0)
--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:23:19 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/13 20:07:06 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/13 20:42:19 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void	ft_putstr(char *str)
 {
-	int	i;
+	int		i;
+	char	newline;
 
+	newline = '\n';
 	i = 0;
 	while (str[i])
 	{
 		write(1, &str[i], 1);
-		write(1, '\n', 1);
 		i++;
 	}
+	write(1, &newline, 1);
 }
 
 int	main(int argc, char *argv[])
