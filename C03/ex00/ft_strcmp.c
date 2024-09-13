9a64f27bc49b37e3ccc269d71dd94db16c92d827
Tatsuya Okuda <hotatsu.world@gmail.com>
2024-09-13 21:48:47 +0900

    C03ex00

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 00:47:31 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/11 18:27:49 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/13 19:29:02 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,38 +28,38 @@ int	ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int	main(void)
-{
-	char	original[] = "ABCDE";
-	char	same[] = "ABCDE";
-	char	diffoneup[] = "BBCDE";
-	char	diffonedown[] = "@BCDEF";
-	char	null[] = "\0";
-	char	del[] = "\x7f";
-	int		result;
+// int	main(void)
+// {
+// 	char	original[] = "ABCDE";
+// 	char	same[] = "ABCDE";
+// 	char	diffoneup[] = "BBCDE";
+// 	char	diffonedown[] = "@BCDEF";
+// 	char	null[] = "\0";
+// 	char	del[] = "\x7f";
+// 	int		result;
 
-	result = strcmp(original, same);
-	printf("strcmp関数 同じケース\t\t\t：%d\n", result);
-	result = strcmp(original, diffoneup);
-	printf("strcmp関数 マイナスに異なるケース\t：%d\n", result);
-	result = strcmp(original, diffonedown);
-	printf("strcmp関数 プラスに異なるケース\t\t：%d\n", result);
-	result = strcmp(del, null);
-	printf("strcmp関数 最大値プラス\t\t\t：%d\n", result);
-	result = strcmp(null, del);
-	printf("strcmp関数 最小値マイナス\t\t：%d\n\n", result);
-	result = ft_strcmp(original, same);
-	printf("挙動確認 同じケース\t\t\t：%d\n", result);
-	result = ft_strcmp(original, diffoneup);
-	printf("挙動確認 マイナスに異なるケース\t\t：%d\n", result);
-	result = ft_strcmp(original, diffonedown);
-	printf("挙動確認 プラスに異なるケース\t\t：%d\n", result);
-	result = ft_strcmp(del, null);
-	printf("挙動確認 最大値プラス\t\t\t：%d\n", result);
-	result = ft_strcmp(null, del);
-	printf("挙動確認 最小値マイナス\t\t\t：%d\n", result);
-	return (0);
-}
+// 	result = strcmp(original, same);
+// 	printf("strcmp関数 同じケース\t\t\t：%d\n", result);
+// 	result = strcmp(original, diffoneup);
+// 	printf("strcmp関数 マイナスに異なるケース\t：%d\n", result);
+// 	result = strcmp(original, diffonedown);
+// 	printf("strcmp関数 プラスに異なるケース\t\t：%d\n", result);
+// 	result = strcmp(del, null);
+// 	printf("strcmp関数 最大値プラス\t\t\t：%d\n", result);
+// 	result = strcmp(null, del);
+// 	printf("strcmp関数 最小値マイナス\t\t：%d\n\n", result);
+// 	result = ft_strcmp(original, same);
+// 	printf("挙動確認 同じケース\t\t\t：%d\n", result);
+// 	result = ft_strcmp(original, diffoneup);
+// 	printf("挙動確認 マイナスに異なるケース\t\t：%d\n", result);
+// 	result = ft_strcmp(original, diffonedown);
+// 	printf("挙動確認 プラスに異なるケース\t\t：%d\n", result);
+// 	result = ft_strcmp(del, null);
+// 	printf("挙動確認 最大値プラス\t\t\t：%d\n", result);
+// 	result = ft_strcmp(null, del);
+// 	printf("挙動確認 最小値マイナス\t\t\t：%d\n", result);
+// 	return (0);
+// }
