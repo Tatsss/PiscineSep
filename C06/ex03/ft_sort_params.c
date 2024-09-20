1d7ba8e872092917bd9bf51772cc0a7e8b26fafd
Tatsuya <>
2024-09-21 03:21:08 +0900

    03

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,15 +6,15 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:23:19 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/14 17:49:15 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/21 03:19:26 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_swap(char *a, char *b)
+void	ft_swap(char **a, char **b)
 {
-	char	temp;
+	char	*temp;
 
 	temp = *a;
 	*a = *b;
@@ -23,16 +23,18 @@ void	ft_swap(char *a, char *b)
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i])
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+	if (*s1 != '\0')
+		return (*s1);
+	if (*s2 != '\0')
+		return (-*s2);
+	return (0);
 }
 
 void	ft_putstr(char *str)
@@ -52,9 +54,9 @@ void	ft_putstr(char *str)
 
 int	main(int argc, char *argv[])
 {
-	int	i;
 	int	n;
 	int	p;
+	int	i;
 
 	n = argc - 1;
 	while (n > 0)
@@ -63,7 +65,7 @@ int	main(int argc, char *argv[])
 		while (argc - 1 > i)
 		{
 			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
-				ft_swap(argv[i], argv[i + 1]);
+				ft_swap(&argv[i], &argv[i + 1]);
 			i++;
 		}
 		n--;
