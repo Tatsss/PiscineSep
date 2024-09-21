52373fd21dec21aed755aaab2495f82979f124be
Tatsuya <>
2024-09-21 23:13:18 +0900

    S

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 11:16:32 by tokuda            #+#    #+#             */
+/*   Updated: 2024/09/21 22:44:39 by tokuda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	count;
+	int	n;
+
+	count = 0;
+	while (str[count])
+	{
+		n = count + 1;
+		if (str[count] == '+' || str[count] == '-')
+			return (-1);
+		while (str[n])
+		{
+			if (str[count] == str[n])
+				return (-1);
+			n++;
+		}
+		count++;
+	}
+	return (count);
+}
+
+int	ft_atoi(char *str, int size, char *base)
+{
+	int	i;
+	int	ans;
+	int	negative;
+
+	i = 0;
+	ans = 0;
+	negative = 1;
+	while (base[i])
+	{
+		if (base[i] == str[i])
+		{
+			return (i);
+		}
+		++i;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative *= -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		ans = ans * size + (str[i] - '0');
+		i++;
+	}
+	return (ans * negative);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	size;
+	int	result;
+
+	result = 0;
+	size = ft_strlen(base);
+	if (size >= 2)
+	{
+		result = ft_atoi(str, size, base);
+	}
+	return (result);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*base10;
+	char	*base02;
+	char	*base16;
+	char	*base08;
+	char	*nullcase;
+	char	*oneletter;
+	char	*sameletter;
+	char	*plus;
+	char	*value;
+
+	base10 = "0123456789";
+	base02 = "01";
+	base16 = "0123456789ABCDEF";
+	base08 = "poneyvif";
+	nullcase = "";
+	oneletter = "A";
+	sameletter = "ponepvif";
+	plus = "+-asfwunoe";
+	value = "a12";
+	// printf("%d", ft_atoi_base(value, base02));
+	// printf("\n");
+	printf("%d", ft_atoi_base(value, base10));
+	printf("\n");
+	// printf("%d", ft_atoi_base(value, base08));
+	// printf("\n");
+	printf("%d", ft_atoi_base(value, base16));
+	printf("\n");
+	printf("%d", ft_atoi_base(value, nullcase));
+	printf("\n");
+	printf("%d", ft_atoi_base(value, oneletter));
+	printf("\n");
+	printf("%d", ft_atoi_base(value, sameletter));
+	printf("\n");
+	printf("%d", ft_atoi_base(value, plus));
+	return (0);
+}
---
+++ C04/ex05/test.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/15 23:14:11 by rariosa-          #+#    #+#             */
+/*   Updated: 2024/09/21 21:53:11 by tokuda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	g_size_base = 0;
+
+int	check_in_base(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == c)
+		{
+			return (i);
+		}
+		++i;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	n;
+	int	x;
+	int	sign;
+
+	i = 0;
+	n = 0;
+	x = 0;
+	sign = 1;
+	while (base[g_size_base] != '\0')
+		++g_size_base;
+	if (str[0] == '-')
+	{
+		sign = -1;
+		++str;
+	}
+	while (str[i] != '\0')
+	{
+		x = check_in_base(str[i], base);
+		if (x >= 0)
+		{
+			n = (n * g_size_base) + x;
+		}
+		++i;
+	}
+	return (n * sign);
+}
+
+int	main(void)
+{
+	printf("%i", ft_atoi_base("a12", "0123456789abcdef"));
+}
