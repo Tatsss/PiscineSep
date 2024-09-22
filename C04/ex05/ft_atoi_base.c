cd4b14662a4f277f8eea6bbabda162d74ece6324
Tatsuya Okuda <hotatsu.world@gmail.com>
2024-09-22 17:13:08 +0900

    04 05update

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -6,11 +6,23 @@
 /*   By: tokuda <tokuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 11:16:32 by tokuda            #+#    #+#             */
-/*   Updated: 2024/09/21 23:32:49 by tokuda           ###   ########.fr       */
+/*   Updated: 2024/09/22 14:15:18 by tokuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+int	check_baseno(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		++i;
+	}
+	return (-1);
+}
 
 int	ft_strlen(char *str)
 {
@@ -34,92 +46,79 @@ int	ft_strlen(char *str)
 	return (count);
 }
 
-int	ft_atoi(char *str, int size, char *base)
+int	ft_atoi_base(char *str, char *base)
 {
 	int	i;
 	int	ans;
+	int	baseno;
 	int	negative;
+	int	size;
 
 	i = 0;
 	ans = 0;
+	baseno = 0;
 	negative = 1;
-	while (base[i])
+	size = ft_strlen(base);
+	if (size >= 2)
 	{
-		if (base[i] == str[i])
-		{
-			printf("base：%d\n", base[i]);
-			printf("str[i]：%d\n", str[i]);
-			printf("i：%d\n", i);
-			return (i);
-		}
-		++i;
-	}
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
+		if (str[0] == '-')
 			negative *= -1;
-		i++;
-	}
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
-	{
-		ans = ans * size + (str[i] - '0');
-		i++;
+		while (str[i])
+		{
+			baseno = check_baseno(str[i], base);
+			if (baseno >= 0)
+				ans = ans * size + baseno;
+			i++;
+		}
 	}
 	return (ans * negative);
 }
 
-int	ft_atoi_base(char *str, char *base)
-{
-	int	size;
-	int	result;
+// #include <stdio.h>
 
-	result = 0;
-	size = ft_strlen(base);
-	printf("長さ：%d\n", size);
-	if (size >= 2)
-	{
-		result = ft_atoi(str, size, base);
-	}
-	return (result);
-}
+// int	main(void)
+// {
+// 	char	*base10;
+// 	char	*base02;
+// 	char	*base16;
+// 	char	*base08;
+// 	char	*nullcase;
+// 	char	*oneletter;
+// 	char	*sameletter;
+// 	char	*plus;
+// 	char	*value;
+// 	char	*value02;
+// 	char	*value10;
+// 	char	*value08;
 
-#include <stdio.h>
+// 	base10 = "0123456789";
+// 	base02 = "01";
+// 	base16 = "0123456789abcdef";
+// 	base08 = "poneyvif";
+// 	nullcase = "";
+// 	oneletter = "A";
+// 	sameletter = "ponepvif";
+// 	plus = "+-asfwunoe";
+// 	value = "a12";
+// 	value02 = "11";
+// 	value10 = "-89";
+// 	value08 = "-fie";
+// 	printf("%d", ft_atoi_base(value02, base02));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value10, base10));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value08, base08));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value, base16));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value, nullcase));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value, oneletter));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value, sameletter));
+// 	printf("\n");
+// 	printf("%d", ft_atoi_base(value, plus));
+// 	return (0);
+// }
 
-int	main(void)
-{
-	char	*base10;
-	char	*base02;
-	char	*base16;
-	char	*base08;
-	char	*nullcase;
-	char	*oneletter;
-	char	*sameletter;
-	char	*plus;
-	char	*value;
-
-	base10 = "0123456789";
-	base02 = "01";
-	base16 = "0123456789abcdef";
-	base08 = "poneyvif";
-	nullcase = "";
-	oneletter = "A";
-	sameletter = "ponepvif";
-	plus = "+-asfwunoe";
-	value = "a12";
-	// printf("%d", ft_atoi_base(value, base02));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, base10));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, base08));
-	// printf("\n");
-	printf("%d", ft_atoi_base(value, base16));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, nullcase));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, oneletter));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, sameletter));
-	// printf("\n");
-	// printf("%d", ft_atoi_base(value, plus));
-	return (0);
-}
+// https://hogehoge.tk/tool/number.html
